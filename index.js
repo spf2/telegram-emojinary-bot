aa46968a2cbe2ebc9b31fafcb266bb87e0a529d5
spf2 <sfarrell@gmail.com>
2016-04-14 15:53:18 -0700

    doh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var emojinary = require('./emojinary');
 
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {'polling': true});
-bot.setWebHook("");
+bot.setWebHook();
 
 var storage = require('node-persist');
 storage.initSync();
